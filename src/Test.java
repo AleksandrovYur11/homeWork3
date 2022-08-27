6b9feb1888a56370ee3d590017fc2fd29753a7e6
Юра <yurochka.aleksandrov.99@mail.ru>
2022-08-27 12:13:29 +0300

    test1

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+
+        int x = 100;
+        double y = 12.4d;
+        double z = x/y;
+        System.out.println(z);
+        if (z > 5){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+
+    }
+}
